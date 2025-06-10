f1a04b2c470159b12278571796ae65f358dada1c
jaimerc01 <jaimeroade@gmail.com>
2025-06-10 21:02:56 +0200

    Creación del proyecto

---
+++ src/main/java/com/hcc/tfm_hcc/HistoriaClinicaComunApplication.java
@@ -0,0 +1,13 @@
+package com.hcc.tfm_hcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoriaClinicaComunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoriaClinicaComunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcc/tfm_hcc/HistoriaClinicaComunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcc.tfm_hcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoriaClinicaComunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
